273a0212ecc2ecaf75e55e9cac1151f7125333b7
Damian Kuroczko <7778327+dkuroczk@users.noreply.github.com>
2023-11-01 19:26:11 +0100

    feat: sg_client + docs

--- pkg/ai/sagemaker.go
+++ pkg/ai/sagemaker.go
@@ -22,6 +22,12 @@ import (
 	"github.com/fatih/color"
 	"github.com/k8sgpt-ai/k8sgpt/pkg/cache"
 	"github.com/k8sgpt-ai/k8sgpt/pkg/util"
+	"encoding/json"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sagemakerruntime"
 )
 
 type SageMakerAIClient struct {
@@ -40,8 +46,69 @@ func (c *SageMakerAIClient) Configure(config IAIConfig, language string) error {
 
 func (c *SageMakerAIClient) GetCompletion(ctx context.Context, prompt string, promptTmpl string) (string, error) {
 	// Create a completion request
-	response := "I am a SageMaker response to the prompt: " + prompt
-	return response, nil
+	// response := "I am a SageMaker response to the prompt: " + prompt
+
+	// Create a new AWS session
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		Config:            aws.Config{Region: aws.String("eu-west-1")},
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	// Create a new SageMaker runtime client
+	svc := sagemakerruntime.New(sess)
+
+	data := map[string]interface{}{
+		"inputs": []interface{}{
+			[]interface{}{
+				map[string]interface{}{
+					"role":    "system",
+					"content": "DEFAULT_PROMPT",
+				},
+				map[string]interface{}{
+					"role":    "user",
+					"content": prompt,
+				},
+			},
+		},
+		"parameters": map[string]interface{}{
+			"max_new_tokens": 256,
+			"top_p":         0.9,
+			"temperature":   0.6,
+		},
+	}
+	// Convert data to []byte
+	bytesData, err := json.Marshal(data)
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// 	return
+	// }
+
+	// Print the []byte representation
+	// fmt.Println(string(bytesData))
+	// Define the endpoint name
+	endpointName := "endpoint-3T1RMjUTnDvW"
+
+	// Create an input object
+	input := &sagemakerruntime.InvokeEndpointInput{
+		Body:           bytesData,
+		EndpointName:   aws.String(endpointName),
+		ContentType:    aws.String("application/json"), // Set the content type as per your model's requirements
+		Accept:         aws.String("application/json"), // Set the accept type as per your model's requirements
+		CustomAttributes: aws.String("accept_eula=true"),
+	}
+
+	// Call the InvokeEndpoint function
+	result, err := svc.InvokeEndpoint(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// The response from the endpoint is available in result.Body
+	// You may need to deserialize the response based on your model's output format
+
+    //fmt.Printf("Response: %s\n", string(result.Body))
+
+
+	return string(result.Body), nil
 }
 
 func (a *SageMakerAIClient) Parse(ctx context.Context, prompt []string, cache cache.ICache, promptTmpl string) (string, error) {
